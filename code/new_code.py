e009895ce9038eea670632fcb7735147decfcb69
Manish Sharma <manish_sharma1604@yahoo.com>
2019-10-27 02:56:01 +0530

    created new_code.py under code directory

---
+++ code/new_code.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python3.7
+
+# =========================================================================================
+#
+# NAME:           new_code.py
+# DESCRIPTION:    Python new code
+# AUTHOR:         Manish Sharma
+# VERSION:        1.0
+# CREATED:        Oct 16, 2019
+#
+# =========================================================================================
+
+print("This is new file added to the branch manish_dev")
+print("GIT assignment")
