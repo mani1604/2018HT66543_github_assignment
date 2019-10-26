cfdb68c5b929f8da68efc39760f889df1ed98daf
Manish Sharma <manish_sharma1604@yahoo.com>
2019-10-27 02:14:23 +0530

    Create module dir and read_json.py under module

---
+++ module/read_json.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python3.7
+
+# =========================================================================================
+#
+# NAME:           read_json.py
+# DESCRIPTION:    Python module to read from json file and print
+# AUTHOR:         Manish Sharma
+# VERSION:        1.0
+# CREATED:        Oct 16, 2019
+#
+# =========================================================================================
+
+import json
+
+with open('/tmp/version_info.json', 'r') as f:
+    version_dict = json.load(f)
+
+print(version_dict["java"]["version"])
